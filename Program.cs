2e43f1595d8e7f37e49a64d661795eb3fac02696
RISHABH MITTAL <ris.23.mit@gmail.com>
2020-01-14 17:00:29 -0500

    Commit all

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MyFirstProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Change is Good");
         }
     }
 }
